486aef5ec0cfcf6041b281aa0aac777d4aaff302
Baruch Lubinsky <baruchlubinsky@gmail.com>
2014-06-12 13:27:58 +0200

    Added higher level function, Create, Save

---
+++ api.go
@@ -0,0 +1,213 @@
+package plotly
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"bytes"
+	"os"
+	"strings"
+)
+
+func init() {
+	username = os.Getenv("PLOTLY_USERNAME")
+	apikey = os.Getenv("PLOTLY_APIKEY")
+	authenticated = true
+	var credentialsFile *os.File
+	files := []string{
+		"~/plotly_credentials.json",
+		"~/.plotly_credentials.json",
+		"plotly_credentials.json",
+		".plotly_credentials.json",
+	}
+	for _, path := range files {
+		file, err := os.Open(path)
+		if err == nil {
+			credentialsFile = file
+		}
+	}
+	if credentialsFile != nil {
+		data, err := ioutil.ReadAll(credentialsFile)
+		if err != nil {
+			panic("Unable to read supplied credential file.")
+		}
+		credentials := struct{
+			Username string
+			Apikey string
+		}{}
+		err = json.Unmarshal(data, &credentials)
+		if err != nil {
+			panic("Badly formatted credentials file: " + credentialsFile.Name())
+		}
+		username = credentials.Username
+		apikey = credentials.Apikey
+	}
+	if username == "" || apikey == "" {
+		authenticated = false
+	}
+}
+
+const ROOTURL = "https://plot.ly/"
+const POSTURL = ROOTURL + "clientresp/"
+const GETURL = ROOTURL + "apigetfile/"
+const IMAGEURL = ROOTURL + "apigenimage/"
+const VERSION = "2.0"
+const PLATFORM = "golang"
+
+var username string
+var apikey string
+var authenticated bool
+
+type Request struct {
+	Un       string
+	Key      string
+	Origin   string
+	Platform string
+	Version  string
+	Args     string
+	Kwargs   string
+}
+
+type Response struct {
+	ErrorMessage    string `json:"error"`
+	Warning  string
+	Message  string
+}
+
+type PostResponse struct {
+	Filename string
+	Url      string
+	Response
+}
+
+type Payload struct {
+	Figure Figure `json:"figure"`
+}
+
+type GetResponse struct {
+	Payload Payload `json:"payload"`
+	Response
+}
+
+type DownloadResponse struct {
+	Payload []byte
+	Response
+}
+
+type Figure struct {
+	Layout interface{} `json:"layout"`
+	Data interface{} `json:"data"`
+}
+
+type Url string
+
+func NewRequest() *Request {
+	var request = Request{
+		Un: username,
+		Key: apikey,
+		Platform: PLATFORM,
+		Version: VERSION,
+	}
+	return &request
+}
+
+func (r *Request) urlEncode() url.Values {
+	v := url.Values{}
+	v.Set("un", r.Un)
+	v.Set("key", r.Key)
+	v.Set("origin", r.Origin)
+	v.Set("platform", r.Platform)
+	v.Set("version", r.Version)
+	v.Set("args", r.Args)
+	v.Set("kwargs", r.Kwargs)
+	return v
+}
+
+func setHeaders(request *http.Request) {
+	request.Header.Set("plotly-username", username)
+	request.Header.Set("plotly-apikey", apikey)
+	request.Header.Set("plotly-version", VERSION)
+	request.Header.Set("plotly-platform", PLATFORM)
+}
+
+func Post(data *Request) (result PostResponse, err error) {
+	checkCredentials()
+	client := http.DefaultClient
+	response, err := client.PostForm(POSTURL, data.urlEncode())
+	if err != nil {
+		return
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &result)
+	return
+}
+
+func Get(id string) (result *GetResponse, err error) {
+	checkCredentials()
+	request, _ := http.NewRequest("GET", GETURL + username + "/" + id, nil)
+	setHeaders(request)
+	client := http.DefaultClient
+	response, err := client.Do(request)
+	if err != nil {
+		return
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &result)
+	return
+}
+
+func Download(figure Figure, filename string) (err error) {
+	data, err := json.Marshal(figure)
+	if err != nil {
+		return
+	}
+	request, _ := http.NewRequest("POST", IMAGEURL, bytes.NewReader(data))
+	setHeaders(request)
+	client := http.DefaultClient
+	response, err := client.Do(request)
+	if err != nil {
+		return
+	}
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	var result = DownloadResponse{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		return
+	}
+	err = ioutil.WriteFile(filename, result.Payload, 0777)
+	return
+}
+
+func (u Url) Id() string {
+	fields := strings.Split(string(u), "/")
+	if len(fields) == 5 {
+		return fields[4]
+	}
+	return ""
+}
+
+func (r Response) Error() string {
+	if r.ErrorMessage != "" {
+		return r.ErrorMessage
+	} else if r.Warning != "" {
+		return r.Warning
+	} else {
+		return "An unspecified error occured at Plot.ly"
+	}
+}
+
+func checkCredentials() {
+	if !authenticated {
+		panic("Unable to connect to API, no credentials have been supplied.")
+	}
+}
---
+++ plot.go
@@ -0,0 +1,59 @@
+package plotly
+
+import(
+  "encoding/json"
+  "fmt"
+)
+
+type Trace struct {
+	X      []interface{} `json:"x"`
+	Y      []interface{} `json:"y"`
+	Name   string        `json:"name"`
+	YAxis  string        `json:"yaxis"`
+	Type   string        `json:"type"`
+	Marker Marker        `json:"marker"`
+	Line   Line          `json:"line"`
+}
+
+type Marker struct {
+	Line    Line    `json:"line"`
+	Opacity float64 `json:"opacity"`
+}
+
+type Line struct {
+	Color string `json:"color"`
+}
+
+func Create(filename string, figure Figure, public bool) (url Url, err error) {
+  request := NewRequest()
+  request.Origin = "plot"
+  args, err := json.Marshal(figure.Data)
+  if err != nil {
+    return
+  }
+  request.Args = string(args)
+  request.Kwargs = fmt.Sprintf(`{"filename":"%v",
+        "fileopt":"overwrite",
+        "world_readable":%v,
+        "layout":%v
+  }`, filename, public, figure.Layout)
+  result, err := Post(request)
+  if err != nil {
+    return
+  }
+  if result.Url == "" {
+    return Url(""), result
+  }
+  return Url(result.Url), nil
+}
+
+func Save(id string, filename string) error {
+  response, err := Get(id)
+  if err != nil {
+    return err
+  } else if response.Payload.Figure.Data == nil {
+    return response
+  }
+  err = Download(response.Payload.Figure, filename)
+  return err
+}
